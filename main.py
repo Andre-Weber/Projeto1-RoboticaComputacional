8b7c700da2fadf2fe8d950b15995e557b180aabb
Andre-Weber <andreew@al.insper.edu.br>
2019-04-18 19:15:44 -0300

    bugs consertados

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ import time
 from geometry_msgs.msg import Twist, Vector3, Pose
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image, CompressedImage, LaserScan
+from std_msgs.msg import UInt8
 from cv_bridge import CvBridge, CvBridgeError
 import cormodule
 import visao_module
+
 
 
 
@@ -26,7 +28,7 @@ media = []
 centro = []
 atraso = 0.5E9 # 1 segundo e meio. Em nanossegundos
 area = 0.0 # Variavel com a area do maior contorno
-viu_car = False
+viu_cat = False
 centro_mnet = []
 
 # Só usar se os relógios ROS da Raspberry e do Linux desktop estiverem sincronizados. 
@@ -34,18 +36,23 @@ centro_mnet = []
 check_delay = False 
 resultados=[]
 
+dados = 0
+
 dados_bumper = None
 
 def bumperzou(dado):
     global dados_bumper
-    print("Numero: ", dado.data)
+    #print("Numero: ", dado.data)
     dados_bumper = dado.data
 
+
+
+
 def scaneou(dado):
-    print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
-    print("Leituras:")
-    print(np.array(dado.ranges).round(decimals=2))
-    dados = dado
+    #print("Faixa valida: ", dado.range_min , " - ", dado.range_max )
+    #print("Leituras:")
+    #print(np.array(dado.ranges).round(decimals=2))
+    dados = dado.ranges[0]
     global dados
 
 
@@ -57,7 +64,7 @@ def roda_todo_frame(imagem):
 	global cv_image
 	global media
 	global centro
-	global viu_car
+	global viu_cat
 	global centro_mnet
 	global resultados
 
@@ -65,7 +72,7 @@ def roda_todo_frame(imagem):
 	imgtime = imagem.header.stamp
 	lag = now-imgtime # calcula o lag
 	delay = lag.nsecs
-	print("delay ", "{:.3f}".format(delay/1.0E9))
+	#print("delay ", "{:.3f}".format(delay/1.0E9))
 	if delay > atraso and check_delay==True:
 		print("Descartando por causa do delay do frame:", delay)
 		return 
@@ -76,8 +83,10 @@ def roda_todo_frame(imagem):
 		centro_mnet, imagem, resultados =  visao_module.processa(cv_image)
 
 		for r in resultados:
-			if r[0] == "car":
-				viu_car = True
+			if r[0] == "cat":
+				viu_cat = True
+			else:
+				viu_cat = False
 
 
 		depois = time.clock()
@@ -109,18 +118,33 @@ if __name__=="__main__":
 	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
 	recebe_bumper = rospy.Subscriber("/bumper", UInt8, bumperzou)
+
+
 	try:
 
 		while not rospy.is_shutdown():
 			vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
-			if len(media) != 0 and len(centro) != 0:
+
+
+			#caso veja um gato
+			if viu_cat:
+				if len(resultados) !=0:
+					
+					vel_back = Twist(Vector3(-0.3,0,0), Vector3(0,0,0))
+					velocidade_saida.publish(vel_back)
+					rospy.sleep(0.1)
+					vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
+					rospy.sleep(0.1)
+
+
+			elif len(media) != 0 and len(centro) != 0:
 				print("Média dos azuis: {0}, {1}".format(media[0], media[1]))
 				print("Centro dos azuis: {0}, {1}".format(centro[0], centro[1]))
 				#print("testeeeeeeeeeeeeeeeeee", centro, media)
 				vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.1))
 
 				dif = media[0] - centro[1]
-		
+
 				if  dif > 60:
 					vel_turn_right = Twist(Vector3(0.1,0,0), Vector3(0,0,-0.5))
 					velocidade_saida.publish(vel_turn_right)
@@ -133,74 +157,79 @@ if __name__=="__main__":
 					vel_front = Twist(Vector3(0.5,0,0), Vector3(0,0,0))
 					velocidade_saida.publish(vel_front)
 					rospy.sleep(0.1)
-			if viu_car:
-				if len(resultados) !=0:
-					
-					vel_front = Twist(Vector3(-0.3,0,0), Vector3(0,0,0))
-					velocidade_saida.publish(vel_front)
-					rospy.sleep(0.1)
-			velocidade_saida.publish(vel)
-			rospy.sleep(0.1)
+
+
+
+
+
+			vel_forw_survive = Twist(Vector3(0.1,0,0), Vector3(0,0,0))
+			vel_back_survive = Twist(Vector3(-0.1,0,0), Vector3(0,0,0))
+			vel_turn_right_survive = Twist(Vector3(0,0,0), Vector3(0,0,1))
+			vel_turn_left_survive = Twist(Vector3(0,0,0), Vector3(0,0,-1))
+			vel_stop = Twist(Vector3(0,0,0), Vector3(0,0,0))
+
+
 			#adicionando laser scan
-			if dados.ranges[0] < 1:
-				velocidade_saida.publish(vel_back)
+			if dados < 0.1:
+				velocidade_saida.publish(vel_back_survive)
 				rospy.sleep(2)
-			if dados.ranges[0] > 1.02:
-				velocidade_saida.publish(vel_forw)
+				velocidade_saida.publish(vel_turn_right_survive)
 				rospy.sleep(2)
+			
+
+
+
+
 			#adicionando bumper
-			vel_forw = Twist(Vector3(0.1,0,0), Vector3(0,0,0))
-        	vel_back = Twist(Vector3(-0.1,0,0), Vector3(0,0,0))
-        	vel_turn_right = Twist(Vector3(0,0,0), Vector3(0,0,1))
-        	vel_turn_left = Twist(Vector3(0,0,0), Vector3(0,0,-1))
-        	vel_stop = Twist(Vector3(0,0,0), Vector3(0,0,0))
+			
+
 			if dados_bumper == 1:
-				velocidade_saida.publish(vel_back)
+				velocidade_saida.publish(vel_back_survive)
 				rospy.sleep(2)
-				velocidade_saida.publish(vel_turn_left)
+				velocidade_saida.publish(vel_turn_left_survive)
 				rospy.sleep(2)
 				velocidade_saida.publish(vel_stop)
 				rospy.sleep(2)
-				velocidade_saida.publish(vel_forw)
-				rospy.sleep(2)
-				velocidade_saida.publish(vel_turn_right)
-				rospy.sleep(2)
-				dados = 0
+				#velocidade_saida.publish(vel_forw_survive)
+				#rospy.sleep(2)
+				#velocidade_saida.publish(vel_turn_right_survive)
+				#rospy.sleep(2)
+				dados_bumper = 0
 			if dados_bumper == 2:
-				velocidade_saida.publish(vel_back)
+				velocidade_saida.publish(vel_back_survive)
 				rospy.sleep(2)
-				velocidade_saida.publish(vel_turn_right)
+				velocidade_saida.publish(vel_turn_right_survive)
 				rospy.sleep(2)
 				velocidade_saida.publish(vel_stop)
 				rospy.sleep(2)
-				velocidade_saida.publish(vel_forw)
-				rospy.sleep(2)
-				velocidade_saida.publish(vel_turn_left)
-				rospy.sleep(2)
+				#velocidade_saida.publish(vel_forw_survive)
+				#rospy.sleep(2)
+				#velocidade_saida.publish(vel_turn_left_survive)
+				#rospy.sleep(2)
 				dados_bumper = 0
 			if dados_bumper == 3:
-				velocidade_saida.publish(vel_forw)
+				velocidade_saida.publish(vel_forw_survive)
 				rospy.sleep(2)
-				velocidade_saida.publish(vel_turn_left)
+				velocidade_saida.publish(vel_turn_left_survive)
 				rospy.sleep(2)
 				velocidade_saida.publish(vel_stop)
 				rospy.sleep(2)
-				velocidade_saida.publish(vel_back)
-				rospy.sleep(2)
-				velocidade_saida.publish(vel_turn_right)
-				rospy.sleep(2)
+				#velocidade_saida.publish(vel_back_survive)
+				#rospy.sleep(2)
+				#velocidade_saida.publish(vel_turn_right_survive)
+				#rospy.sleep(2)
 				dados_bumper = 0
 			if dados_bumper == 4:
-				velocidade_saida.publish(vel_forw)
+				velocidade_saida.publish(vel_forw_survive)
 				rospy.sleep(2)
-				velocidade_saida.publish(vel_turn_right)
+				velocidade_saida.publish(vel_turn_right_survive)
 				rospy.sleep(2)
 				velocidade_saida.publish(vel_stop)
 				rospy.sleep(2)
-				velocidade_saida.publish(vel_back)
-				rospy.sleep(2)
-				velocidade_saida.publish(vel_turn_left)
-				rospy.sleep(2)
+				#velocidade_saida.publish(vel_back_survive)
+				#rospy.sleep(2)
+				#velocidade_saida.publish(vel_turn_left_survive)
+				#rospy.sleep(2)
 				dados_bumper = 0
 	except rospy.ROSInterruptException:
 	    print("Ocorreu uma exceção com o rospy")
